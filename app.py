da99ca57fabda7b25912b67746d82d2c940fb38c
jli <jeffrey.li@teamaol.com>
2017-01-01 22:39:12 -0500

    exec sql

--- app.py
+++ app.py
@@ -25,5 +25,16 @@ def db():
         host=url.hostname,
         port=url.port
     )
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE COMPANY
+           (ID INT PRIMARY KEY     NOT NULL,
+           NAME           TEXT    NOT NULL,
+           AGE            INT     NOT NULL,
+           ADDRESS        CHAR(50),
+           SALARY         REAL);''')
+    print "Table created successfully"
+
+    conn.commit()
+    conn.close()
     return 'everything is good'
 
