08748530ceb4938276c5dfabe94d1d18cde4ae3a
jli <jeffrey.li@teamaol.com>
2017-01-01 22:05:17 -0500

    minor changes

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import psycopg2
+import urlparse
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('db')
+def db():
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return 'everything is good'
+
