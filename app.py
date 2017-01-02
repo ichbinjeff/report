03800b18e7032ab8a1bc3a245dc6d4eda2c10e6b
jli <jeffrey.li@teamaol.com>
2017-01-01 22:31:43 -0500

    done

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 import os
-#import psycopg2
+import psycopg2
 import urlparse
 
 app = Flask(__name__)
 
-# urlparse.uses_netloc.append("postgres")
-# url = urlparse.urlparse(os.environ["DATABASE_URL"])
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 @app.route('/')
 def index():
@@ -16,14 +16,14 @@ def index():
 def hello():
     return 'Hello, World'
 
-# @app.route('db')
-# def db():
-#     conn = psycopg2.connect(
-#         database=url.path[1:],
-#         user=url.username,
-#         password=url.password,
-#         host=url.hostname,
-#         port=url.port
-#     )
-#     return 'everything is good'
+@app.route('db')
+def db():
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return 'everything is good'
 
